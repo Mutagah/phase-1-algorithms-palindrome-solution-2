72770a8125a8be8731f1215fe794c537537d1df2
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-17 15:57:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let startIndex = 0; startIndex < word.length ; startIndex++)
+  {
+    const lastIndex = word.length - 1 - startIndex
+      if(word[startIndex] === word[lastIndex])
+      {
+        return true
+      } else {
+        return false
+      }
+  }
 }
+/*
+Make a function returning true if the word is a palindrome 
+If first letter is same as the last letter and the second letter is the same as the second last letter
+as we continue we the trend up to the middle we should return true for the palindrome
+*/ 
+/*  Add your pseudocode here
+Take our input and iterate from the beginning to the middle while checking
+while checking if the first character matches the last character
+the second character matches the second ast character and so on until we reach the middle.
 
-/* 
-  Add your pseudocode here
+
 */
-
 /*
   Add written explanation of your solution here
 */
